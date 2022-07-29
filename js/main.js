6c93b134cf18535e2752a073b4cd4dba08199e64
takming-dormitory <school@sao.dsmynas.com>
2022-07-29 12:01:51 +0000

    Site updated: 2022-07-29 12:01:49

--- js/main.js
+++ js/main.js
@@ -2,8 +2,13 @@ console.log('%c Power By 唯一 v' + "6.0" + ' %c https://sao-coding.github.io '
 console.log("%c 不錯喔打開console 請打開上面連接多點幾次廣告 ", "color: white; background: #ff9f40; padding:5px 0;");
 console.log("%c 小小強力爆肝驅動~ ", "color: white; background: #ff9f40; padding:5px 0;");
 console.log("%c 留名一下~FB欒文皓~ ", "color: white; background: #2261ff; padding:5px 0;");
-console.log("%c+", "font-size: 1px; padding: 122px 217px; background-image: url(https://i.imgur.com/bULShN1.gif); background-size: contain; background-repeat: no-repeat; color: transparent;");
-console.log("%c+","background-image: url(https://http://takming-dormitory.github.io/images/15屆.jpg); padding: 122px 217px; background-size: contain; background-repeat: no-repeat; color: transparent;");
+console.log("%c+",
+  `font-size: 1px;
+  padding: 122px 217px;
+  background-image: url(https://takming-dormitory.github.io/images/15屆.jpg);
+  background-size: contain;
+  background-repeat: no-repeat;
+  color: transparent;`);
 document.addEventListener('DOMContentLoaded', function () {
   let blogNameWidth, menusWidth, searchWidth, $nav
   let mobileSidebarOpen = false
